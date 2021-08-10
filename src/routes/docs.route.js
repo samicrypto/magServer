a27bc0474fee9689e6caa46dc600f5dbfa6dd894
Saman <saman.na30ri@gmail.com>
2021-08-10 15:51:33 +0430

    007 complate swagger

--- src/routes/docs.route.js
+++ src/routes/docs.route.js
@@ -7,7 +7,8 @@ const router = express.Router();
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+  // apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+  apis: ['src/routes/*.js'],
 });
 
 router.use('/', swaggerUi.serve);
